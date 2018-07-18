09be5696e385debe7f4ce5d17f1711b7d7ae32ec
Zachariah Crowell <zachariahc@Zachariahs-MacBook-Air.local>
2018-07-17 20:20:58 -0600

    initial file add

---
+++ GPS Driver/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  GPS Driver
+//
+//  Created by Zachariah Crowell on 7/17/18.
+//  Copyright © 2018 Zachariah Crowell. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let url = URL(string: "http://www.zachariahcrowell.com/weeklyschedule.html")!
+        webView.load(URLRequest(url: url))
+        
+        // 2
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        toolbarItems = [refresh]
+        navigationController?.isToolbarHidden = false
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
+
+}
+
+
---
+++ GPS DriverTests/GPS_DriverTests.swift
@@ -0,0 +1,36 @@
+//
+//  GPS_DriverTests.swift
+//  GPS DriverTests
+//
+//  Created by Zachariah Crowell on 7/17/18.
+//  Copyright © 2018 Zachariah Crowell. All rights reserved.
+//
+
+import XCTest
+@testable import GPS_Driver
+
+class GPS_DriverTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
